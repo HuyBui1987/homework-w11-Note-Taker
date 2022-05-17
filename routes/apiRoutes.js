23fcf37a5e77d794dbae53a42e78be5170a3c844
HuyBui1987 <86652591+HuyBui1987@users.noreply.github.com>
2022-05-17 19:05:17 +1000

    add folder routes for all routes of the app

---
+++ routes/apiRoutes.js
@@ -0,0 +1,62 @@
+// Dependencies
+const fs = require("fs");
+
+// imported 'uuid' npm package for unique id
+const { v4: uuidv4 } = require('uuid');
+
+// ROUTING
+module.exports = function (app) {
+
+    // API GET Request
+    app.get("/api/notes", (request, response) => {
+
+        // Read 'db.json' file 
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        
+        // Send read data to response of 'GET' request
+        response.json(data);
+    });
+
+
+    // API POST Request
+    app.post("/api/notes", (request, response) => {
+
+        // Extracted new note from request body.  
+        const newNote = request.body;
+
+        // Assigned unique id obtained from 'uuid' package
+        newNote.id = uuidv4();
+
+        // Read data from 'db.json' file
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    
+        // Pushed new note in notes file 'db.json'
+        data.push(newNote);
+
+        // Written notes data to 'db.json' file
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+
+        // Send response
+        response.json(data);
+    });
+
+
+    // API DELETE request
+    app.delete("/api/notes/:id", (request, response) => {
+
+        // Fetched id to delete
+        let noteId = request.params.id.toString();
+
+        // Read data from 'db.json' file
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
+        // filter data to get notes except the one to delete
+        const newData = data.filter( note => note.id.toString() !== noteId );
+
+        // Write new data to 'db.json' file
+        fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+
+        // Send response
+        response.json(newData);
+    });
+};
